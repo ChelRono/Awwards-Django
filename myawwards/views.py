077b2410081b9fccc32e161634ef62f8f0bd22c6
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-06-13 14:56:44 +0300

    added view function for profile

--- myawwards/views.py
+++ myawwards/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import  UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,5 +10,24 @@ def index(request):
 
 
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile) 
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile') # Redirect back to profile page
 
-   return render(request, 'profile.html')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'profile.html', context)
