cecbf284077b47cbcd91e3970ac3b5133e05c747
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-06-14 13:43:17 +0300

    added login template

--- awwards/urls.py
+++ awwards/urls.py
@@ -18,5 +18,6 @@ from django.urls import re_path as url,include
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'',include('myawwards.urls'))
+    url(r'',include('myawwards.urls')),
+    url('accounts/', include('django.contrib.auth.urls')),
 ]
