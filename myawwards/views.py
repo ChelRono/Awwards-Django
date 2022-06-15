fe0d608db6e11c894623814ccf6b32e41d2945fd
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-06-15 15:10:03 +0300

    added update profile

--- myawwards/views.py
+++ myawwards/views.py
@@ -25,23 +25,9 @@ def profile(request):
    
     return render(request, 'profile.html')
 
-def edit_profile(request, username):
-    user = User.objects.get(username=username)
-    if request.method == 'POST':
-        user_form = UpdateUserForm(request.POST, instance=request.user)
-        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
-        if user_form.is_valid() and prof_form.is_valid():
-            user_form.save()
-            prof_form.save()
-            return redirect('profile', user.username)
-    else:
-        user_form = UpdateUserForm(instance=request.user)
-        prof_form = UpdateUserProfileForm(instance=request.user.profile)
-    params = {
-        'user_form': user_form,
-        'prof_form': prof_form
-    }
-    return render(request, 'updateprofile.html', params)
+def edit_profile(request):
+    form = UpdateUserProfileForm()
+    return render(request, 'updateprofile.html',{'form':form})
 
 
 def signup(request):
